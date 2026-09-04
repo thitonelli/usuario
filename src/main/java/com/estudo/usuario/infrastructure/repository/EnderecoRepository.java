d1bb80062b808c72f0e1e32f1012303f22e27347
Thiago T. <thifabris@gmail.com>
2026-09-04 10:32:34 -0300

    adicionando classes secutiry

---
+++ src/main/java/com/estudo/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.estudo.usuario.infrastructure.repository;
+
+import com.estudo.usuario.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ src/main/java/com/estudo/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,10 @@
+package com.estudo.usuario.infrastructure.repository;
+
+import com.estudo.usuario.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+
+}
